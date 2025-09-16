025727d63508afaf1df4a46dbd06299a67d561a3
chasgabster <gabriella.iofe@chasacademy.se>
2025-09-16 16:56:11 +0200

    started POCO-class

--- Account.cs
+++ Account.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Banken
+﻿namespace Banken
 {
     public class Account
     {
+        private int AccountNumber { get; set; }
+        private decimal Balance { get; set; }
+        //private User Owner { get; set; }
+        //private List<Transaction> Transactions { get; set; }
+
+        public Account(int  accountNumber, decimal balance)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Your account number is: {AccountNumber} \nYour balance is: {Balance}");
+        }
     }
+
+
 }
